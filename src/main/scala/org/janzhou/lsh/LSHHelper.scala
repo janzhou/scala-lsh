5a1e17908350df355f64432739e92fc038018663
Jian Zhou <i@janzhou.org>
2017-01-23 17:29:47 -0500

    test Jaccard Similarity

--- src/main/scala/org/janzhou/lsh/LSHHelper.scala
+++ src/main/scala/org/janzhou/lsh/LSHHelper.scala
@@ -1,7 +1,7 @@
 package org.janzhou.LSH
 
 trait LSHHelper extends NumberConversions {
-  val rand = new scala.util.Random(System.nanoTime)
+  private val rand = new scala.util.Random(System.nanoTime)
 
   def hyperplane(dim:Int):HyperplaneLSH[Int] = new HyperplaneLSH(Array.fill(dim)(rand.nextInt))
 
@@ -20,7 +20,7 @@ trait LSHHelper extends NumberConversions {
     data.map(_ - mid)
   }
 
-  def forInt(dim:Int, min:Int, max:Int, repeating:Int):RepeatingLSH[Int] = {
+  def forIntVector(dim:Int, min:Int, max:Int, repeating:Int):RepeatingLSH[Int] = {
     val signature = new HyperplaneSignatureLSH(Array.fill(repeating)(
       seed(dim, min, max)
     ))
@@ -28,7 +28,7 @@ trait LSHHelper extends NumberConversions {
     new RepeatingLSH(signature)
   }
 
-  def forInt(dim:Int, repeating:Int):RepeatingLSH[Int] = {
+  def forIntVector(dim:Int, repeating:Int):RepeatingLSH[Int] = {
     val signature = new HyperplaneSignatureLSH(Array.fill(repeating)(
       seed(dim)
     ))
@@ -36,16 +36,29 @@ trait LSHHelper extends NumberConversions {
     new RepeatingLSH(signature)
   }
 
-  def forIntSignature(dim:Int, size:Int):HyperplaneSignatureLSH[Int] = {
+  def forIntVectorSignature(dim:Int, size:Int):HyperplaneSignatureLSH[Int] = {
     new HyperplaneSignatureLSH(Array.fill(size)(
       seed(dim)
     ))
   }
-  
-  def Cosine(a:Iterable[Int], b:Iterable[Int]):Double = {
+
+  def CosineSimilarity(a:Iterable[Int], b:Iterable[Int]):Double = {
     val a2 = Number.dot(a, a)
     val b2 = Number.dot(b, b)
     val ret = Number.dot(a, b) / (math.sqrt(a2) * math.sqrt(b2))
     if(ret > 0) ret else -ret
+  }
+
+  def forIntSetSignature(dim:Int, max:Int, prime:Int):MinSignatureLSH[Int] = {
+    new MinSignatureLSH(Array.fill(dim)(
+      (rand.nextInt(max), rand.nextInt(prime))
+    ), prime)
+  }
+
+  def SetSimilarity(a:Iterable[Int], b:Iterable[Int]):Double = {
+    val x:Double = a.toStream.filter(b.toStream.contains(_)).length.toDouble
+    val y:Double = b.toStream.filter(a.toStream.contains(_)).length.toDouble
+    val z:Double = ( a ++ b ).toStream.length.toDouble
+    (x+y)/z
   }
 }
--- src/test/scala/org/janzhou/hash/HASHSpec.scala
+++ src/test/scala/org/janzhou/hash/HASHSpec.scala
@@ -31,7 +31,7 @@ class LSHSpec extends FlatSpec with Matchers {
   }
 
   it should "hash int Gaussian LSH" in {
-    val _IntHash = LSH.forInt(4, 100)
+    val _IntHash = LSH.forIntVector(4, 100)
     def IntHash(s:Iterable[Int]){
       val hash = _IntHash(LSH.move(IntMin, IntMax, s))
       println(s"Gaussian LSH($hash):$s")
@@ -40,7 +40,7 @@ class LSHSpec extends FlatSpec with Matchers {
   }
 
   it should "hash int MinMax LSH" in {
-    val _IntHash = LSH.forInt(4, IntMin, IntMax, 100)
+    val _IntHash = LSH.forIntVector(4, IntMin, IntMax, 100)
     def IntHash(s:Iterable[Int]){
       val hash = _IntHash(LSH.move(IntMin, IntMax, s))
       println(s"MinMax LSH($hash):$s")
@@ -48,21 +48,32 @@ class LSHSpec extends FlatSpec with Matchers {
     LSHTest(IntHash)
   }
 
-  def SimilarityTest(IntSignature: Iterable[Int] => Iterable[Int], a:Iterable[Int], b:Iterable[Int]){
+  def SimilarityTest(pref:String, IntSignature: Iterable[Int] => Iterable[Int], Similarity:(Iterable[Int], Iterable[Int])=>Double, a:Iterable[Int], b:Iterable[Int]){
     val hasha = IntSignature(a)
     val hashb = IntSignature(b)
-    val sim1 = LSH.Cosine(hasha, hashb)
-    val sim2 = LSH.Cosine(a, b)
-    println(s"[$sim1, $sim2] LSH($hasha):$a LSH($hashb):$b")
+    val sim1 = Similarity(hasha, hashb)
+    val sim2 = Similarity(a, b)
+
+    println(s"$pref[$sim1, $sim2] LSH($hasha):$a LSH($hashb):$b")
   }
 
-  it should "Get Similarity LSH" in {
-    val _IntSignature = LSH.forIntSignature(8, 3)
+  it should "Get Vector Similarity LSH" in {
+    val _IntSignature = LSH.forIntVectorSignature(8, 3)
     def IntSignature(data:Iterable[Int]) = _IntSignature(data)
 
-    SimilarityTest(IntSignature, Array(1,2,3,4,1,2,3,4), Array(1,2,3,4,1,2,3,4))
-    SimilarityTest(IntSignature, Array(1,2,3,4,9,2,3,4), Array(1,2,3,4,1,2,3,4))
-    SimilarityTest(IntSignature, Array(1,91,3,4,1,132,-3,4), Array(1,2,3,4,1,2,3,4))
-    SimilarityTest(IntSignature, Array(51,2,43,14,211,82,3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("V ", IntSignature, LSH.CosineSimilarity, Array(1,2,3,4,1,2,3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("V ", IntSignature, LSH.CosineSimilarity, Array(1,2,3,4,9,2,3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("V ", IntSignature, LSH.CosineSimilarity, Array(1,91,3,4,1,132,-3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("V ", IntSignature, LSH.CosineSimilarity, Array(51,2,43,14,211,82,3,4), Array(1,2,3,4,1,2,3,4))
+  }
+
+  it should "Get Set Similarity LSH" in {
+    val _IntSignature = LSH.forIntSetSignature(5, 211, 257)
+    def IntSignature(data:Iterable[Int]) = _IntSignature(data)
+
+    SimilarityTest("S ", IntSignature, LSH.SetSimilarity, Array(1,2,3,4,1,2,3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("S ", IntSignature, LSH.SetSimilarity, Array(1,2,3,4,9,2,3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("S ", IntSignature, LSH.SetSimilarity, Array(1,91,3,4,1,132,-3,4), Array(1,2,3,4,1,2,3,4))
+    SimilarityTest("S ", IntSignature, LSH.SetSimilarity, Array(51,2,43,14,211,82,3,4), Array(1,2,3,4,1,2,3,4))
   }
 }
