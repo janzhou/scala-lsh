2d052107dfc5c2bd1d5ed9a2e1e2489038e65309
Jian Zhou <i@janzhou.org>
2017-01-24 19:23:12 -0500

    add MappingLSH

---
+++ src/main/scala/org/janzhou/lsh/MappingLSH.scala
@@ -0,0 +1,16 @@
+package org.janzhou.LSH
+
+/**
+ * Convert a signature to Long type. It is intended to use in conjunction with MinSignatureLSH.
+ */
+class MappingLSH[T <% Number[T]](private val signature: SignatureLSH[T], private val prime:T)
+extends LSH[T] {
+  def apply(data:Iterable[T]):Long = {
+    var ret:Long = 0L
+    signature(data).map(_.##).foreach(v => {
+      val pos = v % 64
+      ret = ret & ( 1L << pos )
+    })
+    ret
+  }
+}
