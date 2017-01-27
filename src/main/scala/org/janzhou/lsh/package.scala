f8f7278af15199fb10c76e2b7f6d911204683db6
Jian Zhou <i@janzhou.org>
2017-01-27 08:55:28 -0500

    install guide

--- src/main/scala/org/janzhou/lsh/package.scala
+++ src/main/scala/org/janzhou/lsh/package.scala
@@ -3,6 +3,13 @@ package org.janzhou
 /**
  * [[https://janzhou.org/lsh/ Locality Sensitive Hashing (LSH)]] is a family of hashing methods that tent to produce the same hash (or signature) for similar items.
  *
+ * =Install=
+ * {{{
+ * libraryDependencies += "org.janzhou" %% "scala-lsh" % "versionNumber"
+ * resolvers += "Jian Zhou Release" at "https://raw.githubusercontent.com/janzhou/mvn-repo/release"
+ * }}}
+ *
+ * =Example=
  * {{{
  * import org.janzhou.LSH
  *
